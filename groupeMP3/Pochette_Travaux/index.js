cc53514c911e60016c8f1e4a21454e51045ffa9a
BenoitMiquey <benoit.miquey@etu.univ-lyon1.Fr>
2018-12-07 15:29:28 +0100

    ajout de l'algo de mise à l'échelle des valeurs

---
+++ groupeMP3/Pochette_Travaux/index.js
@@ -0,0 +1,50 @@
+// La fonction qui permet de nettoyer les valeurs négatives des hauteurs d'un objet JSON
+function clean(tab){
+	var tab = Array.from(tab)
+	for(i=3; i< tab.length ; i= i+4){
+		tab[i] = 0;
+	}
+	tab = tab.filter(function (el) {
+		return el != null;
+	});	
+	return tab;
+}
+
+function getPixelsTab(array){
+	var tmp = [];
+	var i=0;
+		do{
+		//console.log(array[45]);
+		array[i]=([array[i],array[i+1],array[i+2]]);
+		array.splice(i,2);
+		console.log(array.length);
+		i++;
+	}while(array.length >i);
+	console.log(array);
+}
+
+
+
+var getPixels = require("get-pixels");
+
+getPixels("img1.png", function(err, pixels) {
+  if(err) {
+    console.log("Bad image path")
+  }
+  var pixels = pixels.data;
+  
+  //var pixels = JSON.parse(pixels);
+  //pixels = clean(pixels);
+  console.log(pixels.length);
+  //console.log(pixels);
+  var px = clean(pixels);
+  console.log(px);
+  console.log(px.length);
+  console.log(px);
+})
+
+function majorColor(array){
+	if(array.length > 8){
+		majorColor();
+	}
+}
